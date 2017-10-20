19d36f79d0983f7f63230b539c6335f4033a6ad7
Jacob <Bilokij@gmail.com>
2017-10-19 18:26:07 -0700

    Newest

---
+++ GeneticProgram3.lisp
@@ -0,0 +1,43 @@
+isp 2.49
+
+;;(progn
+;; (do until current pool filled ;; init population
+;; create new expr
+;; add expr to current pool )
+;; (do until exceed terminal generation count
+;;; bump generation count
+;; (for each expr in current pool
+;; calc fitness for expr )
+;; save a copy of most fit expr for this generation
+;; (do until no more exprs in current pool
+;; select 2 exprs as parents
+;; remove parents from current pool
+;; select crossover point/node in each parent
+;;make crossed kids
+;; expose each kid to mutattion
+;;add kids to next pool )
+;;current pool = next pool ))
+
+(defun create_random_child ()
+    (setq child())
+        (setf op (random 3))
+	    (if (= op 0) (setf newop '+))
+	        (if (= op 1) (setf newop '-))
+		    (if (= op 2) (setf newop '*))
+		        (setf child (append child (list newop)))
+			    (setf numele (random 3))
+			        (loop while(>= numele 0) do
+				        (setf curnum (random 10))
+					        (nconc child (list curnum))
+						        (decf numele))
+							     )
+
+							     (defun main (n)
+							         (progn
+								     (setq pop ())
+								         (loop while(< (length pop) n) do
+									     (setf pop (append pop (list (create_random_child )))))
+									         )
+										     (write pop))
+										     (main 50)
+
